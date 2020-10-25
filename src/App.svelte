a184179263403d4092d35ab0d81ad467defe76e5
Gilberetzu <euri.herasme@gmail.com>
2020-10-25 16:01:01 -0400

    GlassCard

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import FloatingCard from "./FloatingCard.svelte";
+</script>
+
+<div class="cardHolder">
+	<FloatingCard/>
+	<FloatingCard/>
+	<FloatingCard/>
+	<FloatingCard/>
+	<FloatingCard/>
+	<FloatingCard/>
+</div>
+
+<style>
+	.cardHolder{
+		padding: 100px;
+		display: inline-grid;
+		gap: 10px;
+		grid-template-areas: 'a a a'
+							 'a a a';
+	}
+</style>
